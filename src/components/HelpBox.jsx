be9dab10c051fe0262b88815049382b38a32d43a
david <mario.bargianu@gmail.com>
2025-02-23 17:14:36 +0200

    changed to h2

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -6,7 +6,7 @@ import React from "react";
 function HelpBox({ title, text }) {
     return (
         <article className="help-box">
-            <h2>{title}</h2>
+            <h3>{title}</h3>
             <p>{text}</p>
         </article>
     );
